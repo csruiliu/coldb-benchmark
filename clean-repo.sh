cb0cee799751a76ade951e54646b1cec910353cd
csruiliu <rui.liu@proton.me>
2019-03-05 19:21:14 -0600

    add config.ini

--- clean-repo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#set -x
-
-trap "echo; echo SIGINT/SIGTERM detected. Exiting ...; exit;" SIGINT SIGTERM
-
-PROJECT_PATH_BASE="/home/rui/Development/coldb-benchmark/stu-projects"
-
-rm -rf ${PROJECT_PATH_BASE}
